d95e4a34a4851c0a949a64284a33b8befde18a77
Joel Barnes <oelarnes@gmail.com>
2020-05-13 22:59:18 -0400

    export initialize

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import http from 'http'
 import express from 'express'
 
 import { oclData } from './middleware'
+import { initializeDb } from './db'
 import { dataSync, dataSyncLoop } from './updates'
 
 if (require.main === module) {
@@ -18,4 +19,4 @@ if (require.main === module) {
     console.log('GraphQL server started on port %s', (server.address()).port)
 }
 
-export { oclData, dataSync, dataSyncLoop }
+export { oclData, dataSync, dataSyncLoop, initializeDb }
