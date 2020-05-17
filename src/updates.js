3dbba81ba7da209d39449799bdbb5c902dc2d563
Joel Barnes <oelarnes@gmail.com>
2020-05-17 00:32:48 -0400

    mkdir right

--- src/updates.js
+++ src/updates.js
@@ -171,7 +171,7 @@ async function processAllEventFiles() {
 
     allEventIds.filter(item => !allFolders.includes(item)).forEach(async item => {
         console.log('Creating event folder for %s', item)
-        await fsPromises.mkdir('./data/events/%s', item)
+        await fsPromises.mkdir(`./data/events/${item}`, )
     })
 
     for (const eventId of allFolders) {
